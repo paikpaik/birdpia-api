7a52877f16f35f3b8ca28209f07f2b8a9d09f4cf
paikpaik <woduf9311@gmail.com>
2023-09-07 15:50:23 +0900

    Add: create domain 추가

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,5 @@
 const { User, Domain } = require("../models");
-
+const { v4: uuidv4 } = require("uuid");
 exports.renderLogin = async (req, res, next) => {
   try {
     const user = await User.findOne({
@@ -16,8 +16,15 @@ exports.renderLogin = async (req, res, next) => {
   }
 };
 
-exports.createDomain = async () => {
+exports.createDomain = async (req, res, next) => {
   try {
+    await Domain.create({
+      UserId: req.user.id,
+      host: req.body.host,
+      type: req.body.type,
+      clientSecret: uuidv4(),
+    });
+    res.redirect("/");
   } catch (error) {
     console.error(error);
     next(error);
