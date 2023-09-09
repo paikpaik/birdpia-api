21deedd96901eeb03a6b9fa43c66deabbf2c056e
paikpaik <woduf9311@gmail.com>
2023-09-09 13:22:14 +0900

    Add: token 발급 test 및 나의 게시글 조회, hashtag 검색 조회 api 추가

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ dotenv.config();
 
 const authRouter = require("./routes/auth");
 const indexRouter = require("./routes/index");
+const v1Router = require("./routes/v1");
 
 const passportConfig = require("./passport");
 
@@ -53,6 +54,7 @@ app.use(passport.session());
 
 app.use("/auth", authRouter);
 app.use("/", indexRouter);
+app.use("/v1", v1Router);
 
 app.use((req, res, next) => {
   const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
---
+++ routes/v1.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const { verifyToken } = require("../middlewares");
+const {
+  createToken,
+  tokenTest,
+  getMyPosts,
+  getPostsByHashtag,
+} = require("../controllers/v1");
+
+const router = express.Router();
+
+// /v1/token
+router.post("/token", createToken);
+router.get("/test", verifyToken, tokenTest);
+
+router.get("/posts/my", verifyToken, getMyPosts);
+router.get("/posts/hashtag/:title", verifyToken, getPostsByHashtag);
+
+module.exports = router;
